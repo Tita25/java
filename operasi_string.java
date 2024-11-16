0e3cf21751dd9892cbafd8e37e684b652109722c
Tita25 <titanurlia8@gmail.com>
2024-11-16 13:05:59 +0700

    Add files via upload

---
+++ operasi_string.java
@@ -0,0 +1,18 @@
+// Class Utama
+public class operasi_string {
+  public static void main(String[] args) {
+    
+    // Membuat Objek String
+    String ke1 = "karakter1";
+    String ke2 = "karakter12346";
+      
+    // Menampilkan Output
+    System.out.println("Jumlah angka pertama: " + ke1.length());
+    System.out.println("Jumlah angka kedua: " + ke2.length());
+      
+  }
+}
+
+// OUTPUT //
+// Jumlah angka pertama: 9
+// Jumlah angka kedua: 13
---
+++ pemanggilan_data.java
@@ -0,0 +1,18 @@
+// Class Utama
+public class pemanggilan_data {
+  public static void main(String[] args) {
+    
+    // Membuat Objek String
+    String firstname = "Rahmat";
+    String lastname = "Udin";
+    
+    // Menampilkan Output
+    System.out.println("Nama Depan: " + firstname);
+    System.out.println("Nema Belakang: " + lastname);
+    
+  }
+}
+
+// OUTPUT //
+// Nama Depan: Rahmat
+// Nama Belakang: Udin
